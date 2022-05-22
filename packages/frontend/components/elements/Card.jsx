0f7910d7b29d16a575867f5f90b25a3a48b96720
e-roy <ericroupe@gmail.com>
2022-05-21 21:45:14 -0400

    css and abi error

--- packages/frontend/components/elements/Card.jsx
+++ packages/frontend/components/elements/Card.jsx
@@ -6,17 +6,12 @@ export const Card = ({ children, className }) => {
 
 const CardContainer = styled.div`
   background-color: #fff;
-  border-color: #e6e6e6;
+  border: #e6e6e6 1px solid;
   border-radius: 20px;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 3px 0 rgba(0, 0, 0, 0.06);
   display: flex;
   flex-direction: column;
   min-height: 300px;
-
   padding: 1rem;
-  width: 60%;
-  margin-top: 20px;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
+  margin: 2rem 10rem;
 `;
